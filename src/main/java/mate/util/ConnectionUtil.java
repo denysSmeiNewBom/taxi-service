b78028bf4cc611d3449d72b3220458a2cbfcc3a9
Denys Smei <denys.smei@artcodevision.com>
2021-10-21 09:01:48 +0300

    Remarks of view and ConnectionUtil field change

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -11,10 +11,10 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "URL";
+    private static final String USERNAME = "USER";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String JDBC_DRIVER = "DRIVER";
 
     static {
         try {
